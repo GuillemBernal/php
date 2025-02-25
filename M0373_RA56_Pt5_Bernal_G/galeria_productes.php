72a58ddb85b4117498a8b45af20cb59213903a93
GuillemBernal <gbernal@ies-eugeni.cat>
2025-02-25 10:44:05 +0100

    - canvis activitat 3

--- M0373_RA56_Pt5_Bernal_G/galeria_productes.php
+++ M0373_RA56_Pt5_Bernal_G/galeria_productes.php
@@ -4,7 +4,32 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Galeria d'Imatges</title>
-    <link rel="stylesheet" href="styles.css">
+    <style>
+        .gallery {
+            display: flex;
+            flex-wrap: wrap;
+            gap: 20px;
+            justify-content: center;
+        }
+        .product {
+            border: 1px solid black;
+            padding: 10px;
+            text-align: center;
+            width: 200px;
+            box-shadow: 3px 3px 6px rgba(0,0,0,0.2);
+        }
+        .product img {
+            width: 100%;
+            height: auto;
+        }
+        .product a {
+            text-decoration: none;
+            color: #333;
+            font-weight: bold;
+            display: block;
+            margin-top: 10px;
+        }
+    </style>
 </head>
 <body>
 <section>
@@ -15,7 +40,7 @@
         foreach ($woocommerce->get('products') as $producte) {
             $src_image=$producte->images[0]->src;
             ?>
-            <a class="example-image-link" href="<?php echo $src_image;?>" data-lightbox>
+            <a class="example-image-link" href="<?php echo $src_image;?>" data-lightbox=>
             <?php
         }
         ?>
